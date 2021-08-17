5c1117a30c5fc3fb3489b72b6a07a1847bcbc43a
Michael Maderich <michael.maderich@gmail.com>
2021-08-17 15:25:16 -0400

    A few more adjustments

--- Day2/greeting.js
+++ Day2/greeting.js
@@ -1,25 +1,14 @@
-/*
-
-Use the strings in the arrays to form a sentence like: "Hi, my name is <Name>
+/* Use the strings in the arrays to form a sentence like: "Hi, my name is <Name>
 Be sure to have beginning words of sentences with the proper case.
-
 Use a different greeting word for each gender
 Use "his" for men, "her" for women, "their" for nonbinary.
-
 Use "my" if the isSelf argument is true
-
 */
+
 const greetingWords = ["hello","hi","hey"];
 const prefixes = ["my", "his", "her", "their"];
-//Possible values of "gender" argument are "male", "female", "nonbinary"
 const genders = ["self", "male", "female", "nonbinary"];
 
-/**
- @param {string} phrase
-        String to capitalize
- @returns {string}
-        String with each first letter of word changed to uppercase
-*/
 function firstCharsUpper(phrase) {
     let words = phrase.split(" ");
     words.forEach( (w, i, arr) => {
@@ -41,8 +30,7 @@ function greeting(name, gender, isSelf) {
     let genderPrefix = prefixes[genders.indexOf(gender.toLowerCase())];
     if (genderPrefix === undefined) genderPrefix = "their";
     let salutation = greetingWords[Math.floor(Math.random()*greeting.length)];
-    salutation = firstCharsUpper(salutation);
-    return `${salutation}, ${genderPrefix} name is ${firstCharsUpper(name)}`;
+    return `${firstCharsUpper(salutation)}, ${genderPrefix} name is ${firstCharsUpper(name)}`;
 }
 
 console.log(greeting('Michael', 'male', true));
