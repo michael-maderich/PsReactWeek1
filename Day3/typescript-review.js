33739bf4c916bd4bb6a8c31976e6f4454734f703
Michael Maderich <michael.maderich@gmail.com>
2021-08-18 11:11:16 -0400

    Completed sunnyDay, circle, and rectangle exercises

---
+++ Day3/typescript-review.js
@@ -0,0 +1,23 @@
+var SUNNY_DAY_MESSAGE = "It is sunny out today";
+var isSunny = true;
+var numberSunnyDays = 1;
+console.log(SUNNY_DAY_MESSAGE);
+function hello(name, isSunny) {
+    if (!isSunny)
+        return "Hi, " + name;
+    return "Hi, " + name + "! " + SUNNY_DAY_MESSAGE;
+}
+console.log(hello("Michael", isSunny));
+/**
+ *
+ * @param isSunny If *today* it is sunny or not.
+ * @param numberOfSunnyDays How many days in a row it has been sunny.
+ * @returns Return a string saying whether it's sunny or not. If it's sunny, string declares how many days it has been sunny for.
+ */
+function sunny_day_message(isSunny, numberOfSunnyDays) {
+    if (isSunny)
+        return SUNNY_DAY_MESSAGE + "! There ha" + ((numberOfSunnyDays === 1) ? 's' : 've') + " been " + numberOfSunnyDays + " sunny day" + ((numberOfSunnyDays === 1) ? '' : 's') + "!";
+    return 'It is not sunny out today :(';
+}
+console.log('\n' + sunny_day_message(isSunny, numberSunnyDays));
+console.log('\n' + sunny_day_message(true, 5));
