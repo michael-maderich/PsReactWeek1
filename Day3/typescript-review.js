9fc3aa4751246d5e7898bf1ad44f861c0d80e0ee
Michael Maderich <michael.maderich@gmail.com>
2021-08-19 02:27:12 -0400

    JS Concepts Review Exercise completed, mostly lol

--- Day3/typescript-review.js
+++ Day3/typescript-review.js
@@ -1,4 +1,5 @@
 var SUNNY_DAY_MESSAGE = "It is sunny out today";
+var CLOUDY_DAY_MESSAGE = "It is not sunny out today :(";
 var isSunny = true;
 var numberSunnyDays = 1;
 console.log(SUNNY_DAY_MESSAGE);
@@ -17,7 +18,17 @@ console.log(hello("Michael", isSunny));
 function sunny_day_message(isSunny, numberOfSunnyDays) {
     if (isSunny)
         return SUNNY_DAY_MESSAGE + "! There ha" + ((numberOfSunnyDays === 1) ? 's' : 've') + " been " + numberOfSunnyDays + " sunny day" + ((numberOfSunnyDays === 1) ? '' : 's') + "!";
-    return 'It is not sunny out today :(';
+    return CLOUDY_DAY_MESSAGE;
 }
 console.log('\n' + sunny_day_message(isSunny, numberSunnyDays));
-console.log('\n' + sunny_day_message(true, 5));
+console.log('\n' + sunny_day_message(true, 4));
+console.log('\n' + sunny_day_message(false, 3));
+var obj = { x: 0 };
+// None of the following lines of code will throw compiler errors.
+// Using `any` disables all further type checking, and it is assumed
+// you know the environment better than TypeScript.
+obj.foo();
+obj();
+obj.bar = 1010;
+obj = "hello";
+var n = obj;
--- Day3/typescript-review.ts
+++ Day3/typescript-review.ts
@@ -1,11 +1,12 @@
 const SUNNY_DAY_MESSAGE:string = "It is sunny out today";
+const CLOUDY_DAY_MESSAGE:string = "It is not sunny out today :(";
 let isSunny:boolean = true;
 let numberSunnyDays:number = 1;
 
 console.log(SUNNY_DAY_MESSAGE);
 
-function hello(name:string, isSunny:boolean) {
-    if (!isSunny) return "Hi, "+name;
+function hello(name:string, isSunny:boolean):string {
+    if (!isSunny) return `Hi, ${name}`;
     return `Hi, ${name}! ${SUNNY_DAY_MESSAGE}`;
 }
 
@@ -19,8 +20,19 @@ console.log(hello("Michael", isSunny));
  */
 function sunny_day_message(isSunny:boolean, numberOfSunnyDays:number):string {
     if (isSunny) return `${SUNNY_DAY_MESSAGE}! There ha${(numberOfSunnyDays===1)?'s':'ve'} been ${numberOfSunnyDays} sunny day${(numberOfSunnyDays===1)?'':'s'}!`;
-    return 'It is not sunny out today :(';
+    return CLOUDY_DAY_MESSAGE;
 }
 
 console.log('\n'+sunny_day_message(isSunny, numberSunnyDays));
-console.log('\n'+sunny_day_message(true, 5));
+console.log('\n'+sunny_day_message(true, 4));
+console.log('\n'+sunny_day_message(false, 3));
+
+let obj:any = { x: 0 };
+// None of the following lines of code will throw compiler errors.
+// Using `any` disables all further type checking, and it is assumed
+// you know the environment better than TypeScript.
+obj.foo();
+obj();
+obj.bar = 1010;
+obj = "hello";
+const n: number = obj;
